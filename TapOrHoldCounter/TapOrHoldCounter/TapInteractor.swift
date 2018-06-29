adff77023d5f07369fdd588ad8d9173b35e75fdf
Cassandra Wat <me@robotwholearned.com>
2018-06-28 20:48:05 -0400

    ➕ [2] add viewmodel, interactor, uidelegate from [1]

---
+++ TapOrHoldCounter/TapOrHoldCounter/TapInteractor.swift
@@ -0,0 +1,14 @@
+//
+//  TapInteractor.swift
+//  TapCounter
+//
+//  Created by Cassandra Sandquist on 6/28/18.
+//  Copyright © 2018 Cassandra Sandquist. All rights reserved.
+//
+
+import Foundation
+
+protocol TapInteractor: AnyObject {
+    func addTap()
+    func resetTap()
+}
