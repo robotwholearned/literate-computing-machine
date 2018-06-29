6f4ad94465488817649c31146e9ab3323516907b
Cassandra Wat <me@robotwholearned.com>
2018-06-28 20:36:18 -0400

    🎉 [2: Tap or Hold Counter] Initial Commit.

---
+++ TapOrHoldCounter/TapOrHoldCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapOrHoldCounter
+//
+//  Created by Cassandra Sandquist on 6/28/18.
+//  Copyright © 2018 Cassandra Sandquist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
