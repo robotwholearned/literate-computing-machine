dc514a26b6195748f787e4fe11a9e30b4772c517
Cassandra Wat <me@robotwholearned.com>
2018-06-28 20:46:59 -0400

    💅 [2] add launch title + rename tap view controller

--- TapOrHoldCounter/TapOrHoldCounter/TapViewController.swift
+++ TapOrHoldCounter/TapOrHoldCounter/TapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TapViewController.swift
 //  TapOrHoldCounter
 //
 //  Created by Cassandra Sandquist on 6/28/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
